96203e649f2a0a824761e1cb4e7c3c759fb96352
Andrés López <95057542+andreselc@users.noreply.github.com>
2024-06-20 10:26:38 -0400

    nodemon installed

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hola Mundo');
+    res.send('Hola Mundo actalizado!');
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000	;
 
 app.listen(PORT, () => console.log(`El servidor está corriendo en http://localhost:${PORT}`));
