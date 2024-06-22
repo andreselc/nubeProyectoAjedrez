c313dcf6c9cdbb09d13e7964289f9c5a776c4358
Andrés López <95057542+andreselc@users.noreply.github.com>
2024-06-22 19:37:46 -0400

    Ejs configuration made

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// server.js
 require("dotenv").config();
-
 const express = require("express");
 const db = require("./config/db");
+const path = require("path");
 const { connectToRedis, redisClient } = require("./config/redis");
 
 const main = async () => {
@@ -26,6 +25,16 @@ const main = async () => {
     await redisClient.set('my_key', 'my_val');
     const value = await redisClient.get('my_key');
     await redisClient.del('my_key'); 
+
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "views"));
+    app.use(express.static(path.join(__dirname, "public")));
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}))
+
+    app.get("/", (req, res) => {
+        res.render("index")
+    });
 
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => console.log(`Servidor ejecutándose en http://localhost:${PORT}`));
