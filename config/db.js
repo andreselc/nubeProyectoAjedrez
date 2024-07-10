b6a1f3ca83e26ce639a4fd478030536b6734e733
Andrés López <95057542+andreselc@users.noreply.github.com>
2024-07-10 11:25:21 -0400

    OKD Template configurations made, as well as in databases

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const user = process.env.POSTGRES_USER;
 const password = process.env.POSTGRES_PASSWORD;
-const host = process.env.POSTGRES_HOST;
-const port = process.env.POSTGRES_PORT;
+const host = process.env.POSTGRES_HOST || 'db'; 
+const port = process.env.POSTGRES_PORT || 5432;
 const database = process.env.POSTGRES_DATABASE;
 
 const db = new Client({
@@ -15,6 +15,6 @@ const db = new Client({
     database,
     password,
     port
-  });
+});
 
-module. exports = db;
+module.exports = db;
--- config/redis.js
+++ config/redis.js
@@ -1,19 +1,17 @@
 const redis = require('redis');
-const dotenv = require("dotenv")
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const host = process.env.REDIS_HOST || "localhost"
-
+const host = process.env.REDIS_HOST || 'redis';
 
 const redisClient = redis.createClient({
-    port: 6379,
+    port: process.env.REDIS_PORT || 6379,
     host
 });
 
 redisClient.on("connect", () => {	
     console.log("Conectado a Redis...");
-}
-);
+});
 
 module.exports = redisClient;
